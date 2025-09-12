3f7b5c80f8650e63780d33b09bb79024d2029f66
Your Name <you@example.com>
2025-09-13 02:16:51 +0300

    structure: align to Frappe conventions (module=Neo HCM, move DocType & Pages under neo_hcm)

--- neohcm/config/desktop.py
+++ neohcm/config/desktop.py
@@ -3,10 +3,10 @@ from frappe import _
 def get_data():
     return [
         {
-            "module_name": "NeoHCM",
+            "module_name": "Neo HCM",
             "color": "#2C63D6",
             "icon": "octicon octicon-briefcase",
             "type": "module",
-            "label": _("NeoHCM")
+            "label": _("Neo HCM")
         }
     ]
